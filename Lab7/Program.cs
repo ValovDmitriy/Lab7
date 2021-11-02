6115b7085220f439e8fa9e624b30115a010bfeac
Дмитрий Валов <dmitr.valov@yandex.ru>
2021-11-02 21:10:36 +0300

    Добавьте файлы проекта.

---
+++ Lab7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону треугольника a:");
+            double a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сторону треугольника b:");
+            double b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите сторону треугольника c:");
+            double c = Convert.ToInt32(Console.ReadLine());
+            double S;
+            TriangArea(a, b, c, out S);
+
+            static void TriangArea(double a, double b, double c, out double S)
+            {
+                double p = (a + b + c) / 2;
+                S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                string Sstr = String.Format("{0:f}", S);
+                Console.WriteLine("Площадь треугольника равна {0}", Sstr);
+            }
+        }
+    }
+}
---
+++ Lab7_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба:");
+            double a = Convert.ToInt32(Console.ReadLine());
+            double S;
+            double V;
+            KubeVolume(a, out S, out V);
+
+            static void KubeVolume(double a, out double S, out double V)
+            {
+                V = a*a*a;
+                S = 6*a*a;
+                string Sstr = String.Format("{0:f}", S);
+                string Vstr = String.Format("{0:f}", V);
+                Console.WriteLine("Площадь поверхности куба равна {0}", Sstr);
+                Console.WriteLine("Объем куба равна {0}", Vstr);
+            }
+        }
+    }
+}
